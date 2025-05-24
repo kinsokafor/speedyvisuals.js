d6355a0229f702de737b23a1b15b3e2199d3cf64
Kowebtech Ict <info@koweb.com.ng>
2025-05-24 16:38:34 +0100

    initial commit

---
+++ components/SpreadContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="speedy-layout-container">
+    <!-- Left Section -->
+    <div class="left-section">
+      <div class="inner-container">
+        <div class="logo-container">
+          <img :src="Logo" alt="" />
+        </div>
+        <div class="caption-text">Speedy Visuals 2025</div>
+      </div>
+    </div>
+
+    <!-- Right Section -->
+    <div class="right-section">
+      <div class="inner-right">
+        <div class="form-group">
+          <label for="hj">Description</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="hj">Description</label>
+          <textarea name="" id="" class="form-control"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Logo from "../assets/images/logo.png";
+</script>
+
+<style scoped></style>
